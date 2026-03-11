9f14e7362636342b9c50e059384db154cfa953e1
Jiordi Viera <jiordikengne@gmail.com>
2026-03-11 08:31:56 +0100

    Add Algolia site verification meta tag in production head

--- source/_partials/head.blade.php
+++ source/_partials/head.blade.php
@@ -50,6 +50,7 @@
 
 @if ($page->production)
     <script defer src="https://analytics.jiordiviera.me/script.js" data-website-id="04167809-86e7-4be3-b0ca-72e2b392ee52"></script>
+    <meta name="algolia-site-verification"  content="3F0311C1507C6863" />
 @endif
 
 
